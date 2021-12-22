59d39cb388651cb6d791b8369a31ec698a902d69
roofiahmad <roofiahmadsidiq@gmail.com>
2021-12-23 00:26:36 +0700

    removing margin top of the last bar

--- src/component/ProgressBar/ProgressBar.component.js
+++ src/component/ProgressBar/ProgressBar.component.js
@@ -9,6 +9,7 @@ export class ProgressBar extends PureComponent {
     };
 
 
+    
     renderShipping(){
         const {checkoutStep} = this.props;
         const shippingBar = checkoutStep === 'SHIPPING_STEP' || checkoutStep === 'BILLING_STEP' || checkoutStep === 'DETAILS_STEP';
